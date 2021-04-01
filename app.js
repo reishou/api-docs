5cd9e891d647fe3dcb40fbe88ac6fe7f2be82b51
Reishou <reishou90@gmail.com>
2021-04-01 16:18:14 +0700

    add Dockerfile

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const app = express();
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yml');
+const express = require('express')
+const app = express()
+const port = 3000
 
-var options = {
-  swaggerOptions: {
-    validatorUrl: null
-  }
+require('dotenv').config()
+
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yml')
+
+let options = {
+  //
 };
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.use('/api-docs', swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, options))
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
